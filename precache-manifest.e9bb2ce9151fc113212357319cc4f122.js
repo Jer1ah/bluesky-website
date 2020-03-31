dd5a16dcb70c75a6ffaaed1ed7966ef4eee5e4df
Jeriah Bowers <jeriah97bowers@yahoo.com>
2020-03-30 20:26:11 -0400

    Updates

---
+++ precache-manifest.e9bb2ce9151fc113212357319cc4f122.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80df1fe9e2b2bf86a0b264ab6a49a01c",
+    "url": "/bluesky-website/index.html"
+  },
+  {
+    "revision": "d2ce9de49466a6d8deac",
+    "url": "/bluesky-website/static/css/main.1b3bafd6.chunk.css"
+  },
+  {
+    "revision": "8e46bfe6833663a8abe4",
+    "url": "/bluesky-website/static/js/2.2647d8aa.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/bluesky-website/static/js/2.2647d8aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2ce9de49466a6d8deac",
+    "url": "/bluesky-website/static/js/main.5e44dc58.chunk.js"
+  },
+  {
+    "revision": "b47923f55ac5ae3141e8",
+    "url": "/bluesky-website/static/js/runtime-main.33587cf8.js"
+  },
+  {
+    "revision": "3626d6651f87ded2a6d042bcf22b0e38",
+    "url": "/bluesky-website/static/media/clouds.3626d665.svg"
+  },
+  {
+    "revision": "dc0f2f9ee5f14254526a829237adeedc",
+    "url": "/bluesky-website/static/media/cloudy-night.dc0f2f9e.svg"
+  },
+  {
+    "revision": "ee81fe3e0d69d317b99b868777fffa29",
+    "url": "/bluesky-website/static/media/cloudy.ee81fe3e.svg"
+  },
+  {
+    "revision": "ab4f3dc9bfea105b0932ffdf9e27be1e",
+    "url": "/bluesky-website/static/media/moon.ab4f3dc9.svg"
+  },
+  {
+    "revision": "c59a6525c7a480fd5ff8e7a58dccdbb8",
+    "url": "/bluesky-website/static/media/rain.c59a6525.svg"
+  },
+  {
+    "revision": "66fc4ff71f6e31f60c83e0b82a2f409c",
+    "url": "/bluesky-website/static/media/snow.66fc4ff7.svg"
+  },
+  {
+    "revision": "240fa36a17950ba2ffca82c4d49f0e5a",
+    "url": "/bluesky-website/static/media/sun.240fa36a.svg"
+  },
+  {
+    "revision": "926dad6db147553b559ebf561eea7e26",
+    "url": "/bluesky-website/static/media/thunderstorm.926dad6d.svg"
+  }
+]);
